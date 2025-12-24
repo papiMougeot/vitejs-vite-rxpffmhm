4de1ce19a64c6f8b9b56584616a9b0c5b2a89134
null <null>
2025-12-24 23:43:11 +0100

    Maj du tirage 1905 du 23/12/2025

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -1,17 +1,18 @@
-// =============================================================================
-// DONNÉES DU DERNIER TIRAGE (Modifiable sans toucher au moteur)
-// =============================================================================
+// ======================================================
+// DONNÉES DU DERNIER TIRAGE 1905
+// ======================================================
 
 export const INFOS_TIRAGE = {
-    titre_entete: 'Dernier tirage €Millions enregistré :',
-    numero_et_date: 'N° 1904 – vendredi 19/12/2025',
-    boules: ['17', '21', '39', '43', '44'],
-    etoiles: ['01', '11'],
-    rang: 'Rang : 122 593 690',
-    gagnants: '2 gagnants à 5+1',
-    gain_montant: 'Gain de 338 895,20 €',
-    gain_lieu: 'remporté en EUROPE',
-    my_million: 'My Million N° 9 278 268',
-    prochain_date: 'Prochain Tirage : mardi 23/12/2025',
-    prochain_jackpot: 'Jackpot de 40 000 000 €',
-  };
+  titre_entete: 'Dernier tirage €Millions enregistré :',
+  numero_et_date: 'N° 1905 – mardi 23/12/2025',
+  boules: ['08', '26', '27', '29', '44'],
+  etoiles: ['11', '12'],
+  rang: 'Rang : 82 861 614',
+  gagnants: '6 gagnants à 5+1',
+  gain_montant: 'Gain de 99 593 €',
+  gain_lieu: 'remporté en Europe',
+  my_million: 'My Million N° 27 273',
+  prochain_date: 'Prochain Tirage : vendredi 26/12/2025',
+  prochain_jackpot: 'Jackpot de 53 000 000 €',
+};
+
