cd58221f58b5b59f2f1633a2b24053162f43fde0
null <null>
2025-12-27 21:33:25 +0100

    MAJ Tirage du 26/12/2025

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -1,18 +1,18 @@
-// ======================================================
-// DONNÉES DU DERNIER TIRAGE 1905
-// ======================================================
+// =============================================================================
+// DONNÉES DU DERNIER TIRAGE (Modifiable sans toucher au moteur)
+// =============================================================================
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1905 – mardi 23/12/2025',
-  boules: ['08', '26', '27', '29', '44'],
-  etoiles: ['11', '12'],
-  rang: 'Rang : 82 861 614',
-  gagnants: '6 gagnants à 5+1',
-  gain_montant: 'Gain de 99 593 €',
-  gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 27 273',
-  prochain_date: 'Prochain Tirage : vendredi 26/12/2025',
-  prochain_jackpot: 'Jackpot de 53 000 000 €',
+  numero_et_date: 'N° 1906 – vendredi 26/12/2025',
+  boules: ['12', '22', '32', '36', '48'],
+  etoiles: ['03', '04'],
+  rang: 'Rang : 105 298 600',
+  gagnants: '1 gagnant à 5+2',
+  gain_montant: 'Gain de 54 026 007 €',
+  gain_lieu: 'remporté en Espagne',
+  my_million: 'My Million N° 3 447 031',
+  prochain_date: 'Prochain Tirage : mardi 30/12/2025',
+  prochain_jackpot: 'Jackpot de 17 000 000 €',
 };
 
