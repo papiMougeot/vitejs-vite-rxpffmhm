3050cedbaf497be5f688a3d72f041da935651b25
null <null>
2026-01-03 20:36:23 +0100

    Maj du tirage du 2 janvier 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1907 – mardi 30/12/2025',
-  boules: ['11', '26', '29', '34', '44'],
+  numero_et_date: 'N° 1908 – vendredi 02/01/2026',
+  boules: ['08', '27', '42', '44', '46'],
   etoiles: ['01', '10'],
-  rang: 'Rang : 101 040 729',
-  gagnants: '1 gagnant à 5+1',
-  gain_montant: 'Gain de 539 028 €',
+  rang: 'Rang : 83 104 635',
+  gagnants: '4 gagnants à 5+1',
+  gain_montant: 'Gain de 181 127 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 3 296 221',
-  prochain_date: 'Prochain Tirage : vendredi 02/01/2026',
-  prochain_jackpot: 'Jackpot de 31 000 000 €',
+  my_million: 'My Million N° 5 214 040',
+  prochain_date: 'Prochain Tirage : mardi 06/01/2026',
+  prochain_jackpot: 'Jackpot de 41 000 000 €',
 };
 
