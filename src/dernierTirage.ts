560dec16f8dfa1a7621de6ef0d865db1f58e98f7
null <null>
2026-01-12 20:27:29 +0100

    Dernier tirage du  9 janv 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1909 – mardi 06/01/2026',
-  boules: ['05', '14', '17', '18', '31'],
-  etoiles: ['10', '12'],
-  rang: 'Rang : 54 920 249',
-  gagnants: '3 gagnants à 5',
-  gain_montant: 'Gain de 204 841 €',
+  numero_et_date: 'N° 1910 – vendredi 09/01/2026',
+  boules: ['01', '07', '10', '26', '34'],
+  etoiles: ['02', '04'],
+  rang: 'Rang : 5 167 351',
+  gagnants: '6 gagnants à 5+1',
+  gain_montant: 'Gain de 116 285 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 2 772 217',
-  prochain_date: 'Prochain Tirage : vendredi 09/01/2026',
-  prochain_jackpot: 'Jackpot de 53 000 000 €',
+  my_million: 'My Million N° 1 034 529',
+  prochain_date: 'Prochain Tirage : mardi 13/01/2026',
+  prochain_jackpot: 'Jackpot de 64 000 000 €',
 };
 
