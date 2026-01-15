10fcc82bdb163938dee3f8379859e3bea56b861b
null <null>
2026-01-15 17:10:44 +0100

    Tirage 1911 du 13 janv 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1910 – vendredi 09/01/2026',
-  boules: ['01', '07', '10', '26', '34'],
-  etoiles: ['02', '04'],
-  rang: 'Rang : 5 167 351',
-  gagnants: '6 gagnants à 5+1',
-  gain_montant: 'Gain de 116 285 €',
+  numero_et_date: 'N° 1911 – mardi 13/01/2026',
+  boules: ['06', '10', '18', '44', '47'],
+  etoiles: ['02', '10'],
+  rang: 'Rang : 61 801 297',
+  gagnants: '3 gagnants à 5+1',
+  gain_montant: 'Gain de 187 050 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 1 034 529',
-  prochain_date: 'Prochain Tirage : mardi 13/01/2026',
-  prochain_jackpot: 'Jackpot de 64 000 000 €',
+  my_million: 'My Million N° 2 836 404',
+  prochain_date: 'Prochain Tirage : vendredi 16/01/2026',
+  prochain_jackpot: 'Jackpot de 77 000 000 €',
 };
 
