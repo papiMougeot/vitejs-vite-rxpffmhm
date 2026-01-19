ff2eb96a0f65ff28c6bc851548381e76ff2b3c70
null <null>
2026-01-19 21:35:36 +0100

    MAJ tirage 1912 du 16/01/2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1911 – mardi 13/01/2026',
-  boules: ['06', '10', '18', '44', '47'],
-  etoiles: ['02', '10'],
-  rang: 'Rang : 61 801 297',
+  numero_et_date: 'N° 1912 – vendredi 16/01/2026',
+  boules: ['05', '17', '24', '29', '50'],
+  etoiles: ['05', '10'],
+  rang: 'Rang : 56 315 863',
   gagnants: '3 gagnants à 5+1',
-  gain_montant: 'Gain de 187 050 €',
+  gain_montant: 'Gain de 265 078 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 2 836 404',
-  prochain_date: 'Prochain Tirage : vendredi 16/01/2026',
-  prochain_jackpot: 'Jackpot de 77 000 000 €',
+  my_million: 'My Million N° 7 326 307',
+  prochain_date: 'Prochain Tirage : mardi 20/01/2026',
+  prochain_jackpot: 'Jackpot de 87 000 000 €',
 };
 
