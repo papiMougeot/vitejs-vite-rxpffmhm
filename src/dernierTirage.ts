d7d6833b0b8d9c67745967ca88b218ceac8ca673
null <null>
2026-01-21 11:33:38 +0100

    Maj du tirage 1913 du 20 janvier 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1912 – vendredi 16/01/2026',
-  boules: ['05', '17', '24', '29', '50'],
-  etoiles: ['05', '10'],
-  rang: 'Rang : 56 315 863',
-  gagnants: '3 gagnants à 5+1',
-  gain_montant: 'Gain de 265 078 €',
+  numero_et_date: 'N° 1913 – mardi 20/01/2026',
+  boules: ['11', '18', '19', '22', '50'],
+  etoiles: ['01', '11'],
+  rang: 'Rang : 99 143 164',
+  gagnants: '2 gagnants à 5+1',
+  gain_montant: 'Gain de 295 401 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 7 326 307',
-  prochain_date: 'Prochain Tirage : mardi 20/01/2026',
-  prochain_jackpot: 'Jackpot de 87 000 000 €',
+  my_million: 'My Million N° 6 316 934',
+  prochain_date: 'Prochain Tirage : vendredi 23/01/2026',
+  prochain_jackpot: 'Jackpot de 99 000 000 €',
 };
 
