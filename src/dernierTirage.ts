0f43a1f1be4394014692646efb9437a35e22edd6
null <null>
2026-01-30 08:11:28 +0100

    Maj tirage 1915 du 27/01/2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1913 – mardi 20/01/2026',
-  boules: ['11', '18', '19', '22', '50'],
-  etoiles: ['01', '11'],
-  rang: 'Rang : 99 143 164',
-  gagnants: '2 gagnants à 5+1',
-  gain_montant: 'Gain de 295 401 €',
+  numero_et_date: 'N° 1915 – mardi 27/01/2026',
+  boules: ['04', '23', '42', '43', '47'],
+  etoiles: ['03', '09'],
+  rang: 'Rang : 48 204 777',
+  gagnants: '8 gagnants à 5',
+  gain_montant: 'Gain de 95 070 €',
   gain_lieu: 'remporté en Europe',
-  my_million: 'My Million N° 6 316 934',
-  prochain_date: 'Prochain Tirage : vendredi 23/01/2026',
-  prochain_jackpot: 'Jackpot de 99 000 000 €',
+  my_million: 'My Million N° 6 984 086',
+  prochain_date: 'Prochain Tirage : vendredi 30/01/2026',
+  prochain_jackpot: 'Jackpot de 123 000 000 €',
 };
 
