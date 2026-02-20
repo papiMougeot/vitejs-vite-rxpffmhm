791b8a7c8352f55b9410a7adb8ab0823ec4e17f1
null <null>
2026-02-20 13:04:28 +0100

    MAJ Tirage 1921 du 17/02/2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1920 – vendredi 13/02/2026',
-  boules: ['09', '13', '31', '37', '40'],
-  etoiles: ['06', '09'],
-  rang: 'Rang : 85 949 142',
-  gagnants: '3 gagnants à 5+1',
-  gain_montant: 'Gain de 263 509 €',
+  numero_et_date: 'N° 1921 – mardi 17/02/2026',
+  boules: ['01', '04', '06', '10', '41'],
+  etoiles: ['05', '12'],
+  rang: 'Rang : 2 287 407',
+  gagnants: '5 gagnants à 5+1',
+  gain_montant: 'Gain de 109 156 €',
   gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 4 790 473',
-  prochain_date: 'Prochain Tirage : mardi 17/02/2026',
-  prochain_jackpot: 'Jackpot de 61 000 000 €',
+  my_million: 'My Million N° 7 629 710',
+  prochain_date: 'Prochain Tirage : vendredi 20/02/2026',
+  prochain_jackpot: 'Jackpot exceptionnel de 130 000 000 €',
 };
 
