631a55860690773d7dcc75a9eb60d9dea9765750
null <null>
2026-03-02 10:00:48 +0100

    MAJ du tirage 1924 du 27/02/2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1923 – mardi 24/02/2026',
-  boules: ['10', '27', '40', '43', '47'],
+  numero_et_date: 'N° 1924 – vendredi 27/02/2026',
+  boules: ['14', '24', '27', '39', '42'],
   etoiles: ['06', '10'],
-  rang: 'Rang : 95 815 681',
-  gagnants: '4 gagnants à 5+1',
-  gain_montant: 'Gain de 199 962 €',
-  gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 637 125',
-  prochain_date: 'Prochain Tirage : vendredi 27/02/2026',
-  prochain_jackpot: 'Jackpot 159 000 000 €',
+  rang: 'Rang : 113 848 927',
+  gagnants: '14 gagnants à 5+1',
+  gain_montant: 'Gain de 75 686 €',
+  gain_lieu: 'remporté en Europe dont 5 en France',
+  my_million: 'My Million N° 1 977 609',
+  prochain_date: 'Prochain Tirage : mardi 03/03/2026',
+  prochain_jackpot: 'Jackpot de 174 000 000 €',
 };
 
