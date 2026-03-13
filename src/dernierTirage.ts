366eab3923228eb64addf6f1bbe88673cda06cf3
null <null>
2026-03-13 16:51:39 +0100

    MAJ Tirage 1927 du 10/03/2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1926 – vendredi 06/03/2026',
-  boules: ['15', '16', '19', '28', '37'],
-  etoiles: ['06', '09'],
-  rang: 'Rang : 115 038 840',
-  gagnants: '5 gagnants à 5+1',
-  gain_montant: 'Gain de 233 193 €',
-  gain_lieu: 'remporté en Europe dont 2 en France',
-  my_million: 'My Million N° 5 413 784',
-  prochain_date: 'Prochain Tirage : mardi 10/03/2026',
-  prochain_jackpot: 'Jackpot de 209 000 000 €',
+  numero_et_date: 'N° 1927 – mardi 10/03/2026',
+  boules: ['12', '14', '27', '44', '50'],
+  etoiles: ['04', '12'],
+  rang: 'Rang : 102 704 354',
+  gagnants: '1 gagnant à 5+2',
+  gain_montant: 'Gain de 209 527 211 €',
+  gain_lieu: 'remporté au Royaume Uni',
+  my_million: 'My Million N° 2 808 208',
+  prochain_date: 'Prochain Tirage : vendredi 13/03/2026',
+  prochain_jackpot: 'Jackpot de 17 000 000 €',
 };
 
