6fec51e08e158c41d9d49751f58e0345be50fe92
null <null>
2026-03-20 09:33:44 +0100

    MAJ Tirage 1929 du 17 mars 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1928 – vendredi 13/03/2026',
-  boules: ['13', '17', '26', '41', '48'],
-  etoiles: ['04', '10'],
-  rang: 'Rang : 108 232 116',
-  gagnants: '2 gagnants à 5+1',
-  gain_montant: 'Gain de 415 148 €',
-  gain_lieu: 'remporté en Europe dont aucun en France',
-  my_million: 'My Million N° 612 918',
-  prochain_date: 'Prochain Tirage : mardi 17/03/2026',
-  prochain_jackpot: 'Jackpot de 26 000 000 €',
+  numero_et_date: 'N° 1929 – mardi 17/03/2026',
+  boules: ['05', '17', '28', '33', '41'],
+  etoiles: ['03', '09'],
+  rang: 'Rang : 56 390 097',
+  gagnants: '12 gagnants à 5+1',
+  gain_montant: 'Gain de 42 064 €',
+  gain_lieu: 'remporté en Europe dont 2 en France',
+  my_million: 'My Million N° 6 326 165',
+  prochain_date: 'Prochain Tirage : vendredi 20/03/2026',
+  prochain_jackpot: 'Jackpot de 39 000 000 €',
 };
 
