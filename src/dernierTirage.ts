6a85d9197601518819264e5ba3bc1182e70da3a3
papiMougeot <rogeonalain@hotmail.com>
2026-03-26 20:09:52 +0100

    Update dernierTirage.ts
    MAJ du Tirage 1931 du24 mars 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1930 – vendredi 20/03/2026',
-  boules: ['05', '12', '16', '37', '46'],
-  etoiles: ['08', '10'],
-  rang: 'Rang : 53 929 912',
+  numero_et_date: 'N° 1931 – mardi 24/03/2026',
+  boules: ['12', '16', '17', '18', '27'],
+  etoiles: ['01', '03'],
+  rang: 'Rang : 103 254 758',
   gagnants: '3 gagnants à 5+1',
-  gain_montant: 'Gain de 220 585 €',
-  gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 7 727 907',
-  prochain_date: 'Prochain Tirage : mardi 24/03/2026',
-  prochain_jackpot: 'Jackpot de 50 000 000 €',
+  gain_montant: 'Gain de 172 871 €',
+  gain_lieu: 'remporté en Europe dont 2 en France',
+  my_million: 'My Million N° 9 179 927',
+  prochain_date: 'Prochain Tirage : vendredi 27/03/2026',
+  prochain_jackpot: 'Jackpot de 63 000 000 €',
 };
 
