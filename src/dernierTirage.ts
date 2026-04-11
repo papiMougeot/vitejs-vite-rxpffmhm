51020f5d2305fa4031fafa63a0e29f5325ddfe07
papiMougeot <rogeonalain@hotmail.com>
2026-04-11 17:43:57 +0200

    Update dernierTirage.ts après le tirage 1936 du 10 avril 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1935 – mardi 07/04/2026',
-  boules: ['11', '14', '19', '36', '49'],
-  etoiles: ['06', '07'],
-  rang: 'Rang : 97 647 706',
-  gagnants: '5 gagnants à 5+1',
-  gain_montant: 'Gain de 113 717 €',
-  gain_lieu: 'remporté en Europe dont 2 en France',
-  my_million: 'My Million N° 6 692 859',
-  prochain_date: 'Prochain Tirage : vendredi 10/04/2026',
-  prochain_jackpot: 'Jackpot de 105 000 000 €',
+  numero_et_date: 'N° 1936 – vendredi 10/04/2026',
+  boules: ['10', '13', '14', '38', '41'],
+  etoiles: ['06', '09'],
+  rang: 'Rang : 91 574 454',
+  gagnants: '3 gagnants à 5+1',
+  gain_montant: 'Gain de 270 960 €',
+  gain_lieu: 'remporté en Europe dont aucun en France',
+  my_million: 'My Million N° 910 623',
+  prochain_date: 'Prochain Tirage : mardi 14/04/2026',
+  prochain_jackpot: 'Jackpot de 115 000 000 €',
 };
 
