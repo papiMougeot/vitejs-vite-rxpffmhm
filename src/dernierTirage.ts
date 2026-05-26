72ae15218b8101e4863160be29c83f8f302ef607
null <null>
2026-05-26 11:29:52 +0200

    MAJ tirage 1948 du 22 mai 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1947 – mardi 19/05/2026',
-  boules: ['02', '12', '20', '38', '45'],
-  etoiles: ['02', '05'],
-  rang: 'Rang : 21 681 608',
-  gagnants: '3 gagnants à 5+1',
-  gain_montant: 'Gain de 195 442 €',
-  gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 7 938 733',
-  prochain_date: 'Prochain Tirage : vendredi 22/05/2026',
-  prochain_jackpot: 'Jackpot de 105 000 000 €',
+  numero_et_date: 'N° 1948 – vendredi 22/05/2026',
+  boules: ['06', '22', '26', '31', '37'],
+  etoiles: ['05', '08'],
+  rang: 'Rang : 66 664 529',
+  gagnants: '5 gagnants à 5+1',
+  gain_montant: 'Gain de 157 970 €',
+  gain_lieu: 'remporté en Europe dont 1 enFrance',
+  my_million: 'My Million N° 7 352 189',
+  prochain_date: 'Prochain Tirage : mardi 26/05/2026',
+  prochain_jackpot: 'Jackpot de 115 000 000 €',
 };
 
