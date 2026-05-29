b647798a9ad67e36761b743119c7a0f11b255036
null <null>
2026-05-29 17:08:45 +0200

    MAJ Tirage 1949 du 26 mai 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1948 – vendredi 22/05/2026',
-  boules: ['06', '22', '26', '31', '37'],
-  etoiles: ['05', '08'],
-  rang: 'Rang : 66 664 529',
-  gagnants: '5 gagnants à 5+1',
-  gain_montant: 'Gain de 157 970 €',
-  gain_lieu: 'remporté en Europe dont 1 enFrance',
-  my_million: 'My Million N° 7 352 189',
-  prochain_date: 'Prochain Tirage : mardi 26/05/2026',
-  prochain_jackpot: 'Jackpot de 115 000 000 €',
+  numero_et_date: 'N° 1949 – mardi 26/05/2026',
+  boules: ['06', '23', '25', '35', '37'],
+  etoiles: ['06', '12'],
+  rang: 'Rang : 66 843 729',
+  gagnants: '1 gagnant à 5+1',
+  gain_montant: 'Gain de 624 963 €',
+  gain_lieu: 'remporté en FRANCE',
+  my_million: 'My Million N° 1 791 377',
+  prochain_date: 'Prochain Tirage : vendredi 29/05/2026',
+  prochain_jackpot: 'Jackpot de 129 000 000 €',
 };
 
