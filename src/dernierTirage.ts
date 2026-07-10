2734fe52dd7772b377cd9c35158aa6aba0533f40
null <null>
2026-07-10 16:40:55 +0200

    MAJ du tirage 1961 du 7 juillet 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1960 – vendredi 03/07/2026',
-  boules: ['02', '12', '17', '25', '39'],
-  etoiles: ['01', '02'],
-  rang: 'Rang : 21 573 619',
-  gagnants: '1 gagnant à 5+2',
-  gain_montant: 'Gain de 80 220 498 €',
-  gain_lieu: 'remporté en Belgique',
-  my_million: 'My Million N° 4 648 307',
-  prochain_date: 'Prochain Tirage : mardi 07/07/2026',
-  prochain_jackpot: 'Jackpot de 17 000 000 €',
+  numero_et_date: 'N° 1961 – mardi 07/07/2026',
+  boules: ['05', '29', '33', '45', '47'],
+  etoiles: ['05', '08'],
+  rang: 'Rang : 58 761 293',
+  gagnants: '3 gagnants à 5+1',
+  gain_montant: 'Gain de 150 510 €',
+  gain_lieu: 'remporté en Europe dont aucun en France',
+  my_million: 'My Million N° 1 376 956',
+  prochain_date: 'Prochain Tirage : vendredi 10/07/2026',
+  prochain_jackpot: 'Jacqpot de 29 000 000 €',
 };
 
