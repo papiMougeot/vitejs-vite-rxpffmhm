8433c670d36d026e75f2d9347f9f7f309920673c
null <null>
2026-07-10 10:17:22 +0200

    MAJ du tirage 1960 du 3 juillet 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1959 – mardi 30/06/2026',
-  boules: ['01', '08', '37', '44', '48'],
-  etoiles: ['02', '06'],
-  rang: 'Rang : 6 576 387',
-  gagnants: '2 gagnants à 5+1',
-  gain_montant: 'Gain de 268 527 €',
-  gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 6 112 890',
-  prochain_date: 'Prochain Tirage : vendredi 03/07/2026',
-  prochain_jackpot: 'Jackpot vde 80 000 000 €',
+  numero_et_date: 'N° 1960 – vendredi 03/07/2026',
+  boules: ['02', '12', '17', '25', '39'],
+  etoiles: ['01', '02'],
+  rang: 'Rang : 21 573 619',
+  gagnants: '1 gagnant à 5+2',
+  gain_montant: 'Gain de 80 220 498 €',
+  gain_lieu: 'remporté en Belgique',
+  my_million: 'My Million N° 4 648 307',
+  prochain_date: 'Prochain Tirage : mardi 07/07/2026',
+  prochain_jackpot: 'Jackpot de 17 000 000 €',
 };
 
