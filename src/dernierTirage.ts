3e83bc337281ee40af72ae05f249e62bc608effb
null <null>
2026-07-20 18:23:05 +0200

    MAJ du tirage 1964 du 17 juillet 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1963 – mardi 14/07/2026',
-  boules: ['10', '19', '37', '42', '47'],
-  etoiles: ['09', '12'],
-  rang: 'Rang : 94 312 017',
-  gagnants: '2 gagnants à 5+1',
-  gain_montant: 'Gain de 238 156 €',
-  gain_lieu: 'remporté en Europe dont 1 en France',
-  my_million: 'My Million N° 3 290 687',
-  prochain_date: 'Prochain Tirage : vendredi 17/07/2026',
-  prochain_jackpot: 'Jackpot de 50 000 000 €',
+  numero_et_date: 'N° 1964 – vendredi 17/07/2026',
+  boules: ['12', '21', '23', '34', '40'],
+  etoiles: ['09', '10'],
+  rang: 'Rang : 104 940 787',
+  gagnants: '7 gagnants à 5',
+  gain_montant: 'Gain de 111 131 €',
+  gain_lieu: 'remporté en Europe dont 3 en France',
+  my_million: 'My Million N° 6 384 778',
+  prochain_date: 'Prochain Tirage : mardi 21/07/2026',
+  prochain_jackpot: 'Jackpot de 59 000 000 €',
 };
 
