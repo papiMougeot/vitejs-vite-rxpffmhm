ad84aaf399e15a14e3a3dfef8d146e71aa4bbf4f
null <null>
2026-07-30 13:23:38 +0200

    MAJ du tirage 1967 du 28 juillet 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1966 – vendredi 24/07/2026',
-  boules: ['08', '10', '30', '36', '47'],
-  etoiles: ['01', '04'],
-  rang: 'Rang : 77 580 825',
+  numero_et_date: 'N° 1967 – mardi 28/07/2026',
+  boules: ['05', '07', '24', '30', '49'],
+  etoiles: ['10', '11'],
+  rang: 'Rang : 50 872 798',
   gagnants: '1 gagnant à 5+1',
-  gain_montant: 'Gain de 645 724 €',
-  gain_lieu: 'remporté en France',
-  my_million: 'My Million N° 8 263 887',
-  prochain_date: 'Prochain Tirage : mardi 28/07/2026',
-  prochain_jackpot: 'Jackpot de 78 000 000 €',
+  gain_montant: 'Gain de 522 576 €',
+  gain_lieu: 'remporté en Europe dont aucun en France !',
+  my_million: 'My Million N° 4 725 470',
+  prochain_date: 'Prochain Tirage : vendredi 31/07/2026',
+  prochain_jackpot: 'Jackpot de 89 000 000 €',
 };
 
