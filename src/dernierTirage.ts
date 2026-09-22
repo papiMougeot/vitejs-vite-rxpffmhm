b0cee46d096438be7278202637676dafc235f70c
null <null>
2026-09-22 17:54:01 +0200

    MAJ du tirage 1982 du 18 septembre 2026

--- src/dernierTirage.ts
+++ src/dernierTirage.ts
@@ -4,15 +4,15 @@
 
 export const INFOS_TIRAGE = {
   titre_entete: 'Dernier tirage €Millions enregistré :',
-  numero_et_date: 'N° 1981 – mardi 15/09/2026',
-  boules: ['10', '16', '18', '22', '28'],
-  etoiles: ['06', '10'],
-  rang: 'Rang : 92 995 039',
-  gagnants: '7 gagnants à 5+1',
-  gain_montant: 'Gain de 63 611 €',
-  gain_lieu: 'remporté en Europe dont 5 en France',
-  my_million: 'My Million non disponible',
-  prochain_date: 'Prochain Tirage : vendredi 18/09/2026',
-  prochain_jackpot: 'Jackpot de 29 000 000 €',
+  numero_et_date: 'N° 1982 – vendredi 18/09/2026',
+  boules: ['08', '17', '25', '30', '44'],
+  etoiles: ['02', '03'],
+  rang: 'Rang : 80 828 562',
+  gagnants: '3 gagnants à 5+1',
+  gain_montant: 'Gain de 198 228 €',
+  gain_lieu: 'remporté en Europe dont 1 en France',
+  my_million: 'My Million N° 9 756 323',
+  prochain_date: 'Prochain Tirage : mardi 22/09/2026',
+  prochain_jackpot: 'Jackpot 37 000 000 €',
 };
 
